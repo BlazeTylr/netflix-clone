9cb833cf1859844291ecfd7bdbc1ec09ea4c0f16
BlazeTylr <blztylr@gmail.com>
2023-02-07 17:04:53 +0000

    update

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import SavedShows from '../components/SavedShows';
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className=" w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/862cc171-8df5-418c-886f-2aaf767ae159/2e8a8624-17c0-41a2-b6ad-de55d7ef37d0/GB-en-20230130-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
 };
 
 export default Account;
